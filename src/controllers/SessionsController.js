6da7abe137fda5296c1e11dfb13d9a5fed4e2eb3
Monica Vaz <contatomonicavaz@gmail.com>
2022-07-26 11:20:54 -0300

    config jwt

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,8 @@
 const knex = require('../database/knex');
 const AppError = require('../utils/AppError');
+const authConfig = require('../configs/auth');
+const { sign } = require('jsonwebtoken');
+const { compare } = require('bcryptjs');
 
 class SessionsController {
   async create(request, response) {
@@ -11,7 +14,19 @@ class SessionsController {
       throw new AppError('Email e/ou senha incorreta', 401);
     }
 
-    return response.json(user);
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('Email e/ou senha incorreta', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return response.json({ user, token });
   }
 }
 
