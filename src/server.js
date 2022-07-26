bf5d869e79682a8dfeef157c6ef113a2acfa2046
Monica Vaz <contatomonicavaz@gmail.com>
2022-07-26 16:31:05 -0300

    add multer, config user avatar

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('express-async-errors');
 const express = require('express');
 const routes = require('./routes');
+const uploadConfig = require('./configs/upload');
 
 const AppError = require('./utils/AppError');
 
@@ -11,6 +12,9 @@ migrationsRun();
 const app = express();
 
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
+
 app.use(routes);
 
 app.use((error, request, response, next) => {
